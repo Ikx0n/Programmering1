78d285234305bc31e9d606c17458bcb2d4fcc735
Ikx0n <Samuel.khan@sundbyberg.it-gymnasiet.se>
2017-08-29 11:15:26 +0200

    Lektion2
    Bil

---
+++ src/lektion2/Car1.java
@@ -0,0 +1,12 @@
+package lektion2;
+
+public class Car1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ANTALET_RATTAR = 1 ;
+		
+		System.out.println(ANTALET_RATTAR);
+	}
+
+}
