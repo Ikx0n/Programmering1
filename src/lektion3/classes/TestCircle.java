631ffcffd0f8e617c3ca88996acd17ebfc77bfc9
Ikx0n <Samuel.khan@sundbyberg.it-gymnasiet.se>
2017-09-05 11:21:08 +0200

    dankmemes

---
+++ src/lektion3/classes/TestCircle.java
@@ -0,0 +1,13 @@
+package lektion3.classes;
+
+public class TestCircle {
+
+	public static void main(String[] args) {
+		Circle dankmemes = new Circle();
+		
+		
+		System.out.println(dankmemes.color);
+		
+	}
+
+}
