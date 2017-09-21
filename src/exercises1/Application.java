f0d232d006ceef668b10caa099c445b007dc54e0
Ikx0n <Samuel.khan@sundbyberg.it-gymnasiet.se>
2017-09-21 11:41:56 +0200

    Exercises

---
+++ src/exercises1/Application.java
@@ -0,0 +1,15 @@
+package exercises1;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// skriver Dankus Memus som en introduktion till nästa del
+		System.out.println("Dankus Memus"); 
+	
+		// Sätter namnet på person till Samme. Är egentligen John Doe i classen.
+		 Person myPerson = new Person( "Samme" );
+		 // Säger "my name is" sedan så tar den namnen ovan och lägger till det.
+		 System.out.println("my name is " + myPerson.getName());
+	}
+
+}
---
+++ src/exercises1/Person.java
@@ -0,0 +1,19 @@
+package exercises1;
+
+public class Person {
+	//sätter namnet på person
+	private String name = "John Doe";
+	// gör person public
+	 public Person() {
+	
+	 }
+
+	 public Person(String name) {
+		 this.name = name;
+	 }
+	 public String getName() {
+		 return this.name;
+	 }
+	 
+}
+	 
