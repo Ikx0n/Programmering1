06aa0fe2fffefa27c2b4ce53993acf18225c837a
Ikx0n <Samuel.khan@sundbyberg.it-gymnasiet.se>
2017-11-27 09:53:45 +0100

    Quick maths

---
+++ src/nazi/Main.java
@@ -0,0 +1,27 @@
+package nazi;
+import java.awt.color.*;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		printBoard();
+/**
+ * Print the game board boi
+ */
+	}
+
+	private static void printBoard() {
+		JOptionPane.showMessageDialog(null,"message", "Board", -1);
+	String name = JOptionPane.showInputDialog("Enter your name:");
+	JOptionPane.showInputDialog("Din mamma", "Luktar curry");
+		JOptionPane.showMessageDialog(null, "You shall henceforth be referred to as " + name);
+
+	}
+	
+	
+	
+	
+
+}
